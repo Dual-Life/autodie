09c70b617a0b6469305ce51c99626554fb734b4d
Paul Fenwick <pjf@perltraining.com.au>
2009-05-11 18:33:57 +1000

    Hints provider class complains loudly if we call its method.

--- lib/autodie/hints/provider.pm
+++ lib/autodie/hints/provider.pm
@@ -1,5 +1,11 @@
 package autodie::hints::provider;
 
+sub AUTODIE_HINTS {
+    my ($class) = @_;
+    die "$class inherits from autodie::hints::provider but does not define its own AUTODIE_HINTS() method, or tries to call this method in its parent class.  Please create an AUTODIE_HINTS() method for this class, or remove its inheritance on autodie::hints::provider";
+
+}
+
 # Dummy package for inheritance.
 
 1;
