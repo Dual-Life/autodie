06907d04f45302364a36e738024e366c0ebd15f6
Paul Fenwick <pjf@perltraining.com.au>
2010-02-27 13:51:55 +1100

    Bumped version to 2.10.  Release!

--- lib/autodie/exception.pm
+++ lib/autodie/exception.pm
@@ -14,7 +14,7 @@ use overload
 
 use if ($] >= 5.010), overload => '~~'  => "matches";
 
-our $VERSION = '2.09';
+our $VERSION = '2.10';
 
 my $PACKAGE = __PACKAGE__;  # Useful to have a scalar for hash keys.
 
--- lib/autodie/hints.pm
+++ lib/autodie/hints.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use constant PERL58 => ( $] < 5.009 );
 
-our $VERSION = '2.09';
+our $VERSION = '2.10';
 
 =head1 NAME
 
